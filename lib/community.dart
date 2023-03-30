2a58065ab852ade89dcd09a3e4cf88652e59076b
Nuri Hyun <nurihyun2001@gmail.com>
2023-03-29 23:52:29 -0400

    Denison Dining Logo Updated

--- lib/community.dart
+++ lib/community.dart
@@ -12,12 +12,12 @@ import 'package:like_button/like_button.dart';
 
 class TestPage extends StatefulWidget {
   const TestPage({Key? key}) : super(key: key);
-
   @override
   State<TestPage> createState() => _TestPageState();
 }
 
 class _TestPageState extends State<TestPage> {
+  int count = 0;
   ScrollController listScrollController = ScrollController();
   @override
   WebViewController controller = WebViewController()
--- lib/toggle_buttons.dart
+++ lib/toggle_buttons.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/Cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HeartButton extends StatefulWidget {
@@ -8,17 +7,30 @@ class HeartButton extends StatefulWidget {
 
 class _HeartButtonState extends State<HeartButton> {
   bool _isPressed = false;
+  int _count = 0;
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      icon: _isPressed ? Icon(Icons.favorite) : Icon(Icons.favorite_border),
-      color: _isPressed ? Colors.red : null,
-      onPressed: () {
-        setState(() {
-          _isPressed = !_isPressed;
-        });
-      },
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        IconButton(
+          icon: _isPressed ? Icon(Icons.favorite) : Icon(Icons.favorite_border),
+          color: _isPressed ? Colors.red : null,
+          onPressed: () {
+            setState(() {
+              _isPressed = !_isPressed;
+              if (_isPressed) {
+                _count++;
+              } else {
+                _count--;
+              }
+            });
+          },
+        ),
+        SizedBox(width: 8),
+        Text(_count.toString()),
+      ],
     );
   }
 }
